7faccb6cce8730b4bc1a9ab1c683274842795cf7
MirageM <imiragesz@gmail.com>
2024-10-06 13:30:42 -0400

    if(s_count[i] != t_count[i])

--- Arrays & Hashing/isAnagram46.cpp
+++ Arrays & Hashing/isAnagram46.cpp
@@ -5,6 +5,10 @@ public:
         unordered_map<char,int> s_count;
         unordered_map<char,int> t_count;
         for(int i = 0; i < s.size(); i++){
+            s_count[s[i]]++;
+            t_count[t[i]]++;
+        }
+        for(int i = 0; i < s_count.size(); i++){
             if(s_count[i] != t_count[i]){
                 return false;
             }
