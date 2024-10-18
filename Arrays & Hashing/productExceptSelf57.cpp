0bf0e21db9d7035881b8aeea2869acb8a7e2e6bd
MirageM <imiragesz@gmail.com>
2024-10-17 21:20:33 -0400

    result[i] = result[i] * postfix; postfix = postfix * nums[i];

---
+++ Arrays & Hashing/productExceptSelf57.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int prefix = 1;
+        for(int i = 0; i < nums.size(); i++){
+            result[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+        int postfix = 1;
+        for(int i = nums.size() - 1; i >= 0; i--){
+            result[i] = result[i] * postfix;
+            postfix = postfix * nums[i];
+        }
+        return result;
+
+    }
+};
