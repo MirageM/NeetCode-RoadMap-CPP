061b89c2e5c61696c9ed9d0f19da52e5c292a0f2
Mirage <59549186+MirageM@users.noreply.github.com>
2025-12-07 13:16:43 -0500

    hset.insert({nums[i]);

---
+++ Arrays & Hashing/hasDuplicate91.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> hset;
+        for(int i = 0; i < nums.size(); i++){
+            if(hset.find(nums[i]) != hset.end()){
+                return true;
+            }
+            hset.insert({nums[i]});
+        }
+        return false;
+    }
+};
